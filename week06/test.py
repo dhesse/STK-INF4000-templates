683ef7b57f4034947d26fe987022613f3f3608b9
Dirk Hesse <herr.dirk.hesse@gmail.com>
2017-02-18 20:01:43 +0100

    More week 6 tests.

--- week06/test.py
+++ week06/test.py
@@ -1,7 +1,24 @@
-from homework import return_unique, fib, safe_call, curve_predict
+from homework import return_unique, fib, safe_call, curve_predict, lin_reg_coefs, relevant_features
 import unittest
 import numpy as np
+import pandas as pd
 
+class TestLinRegCoef(unittest.TestCase):
+    def test_known_values(self):
+        x = range(10)
+        y = [i**2 + 2*i + 42 for i in x]
+        data = pd.DataFrame({'x': x, 'y': y})
+        res = lin_reg_coefs(data, 'x', 'y', 2)
+        np.testing.assert_allclose(res, [42, 2, 1])
+
+class TestRelevantFeatures(unittest.TestCase):
+    def test_known_values(self):
+        from sklearn.datasets import make_regression
+        X,y, coef = make_regression(200, 15, 15, coef=True)
+        np.testing.assert_equal(relevant_features(X, y),
+                                coef != 0.0)
+
+        
 class TestReturnUnique(unittest.TestCase):
     def test_ru(self):
         self.assertItemsEqual('abcd',
