17bc08e3ce2bbdb8f00e76f55bf1403c34e0ef1b
Dirk Hesse <herr.dirk.hesse@gmail.com>
2017-05-07 21:50:32 +0200

    Removing solutions.

--- week14/homework.py
+++ week14/homework.py
@@ -1,8 +1,3 @@
-from nltk import bigrams as nl_bigrams
-from nltk.probability import FreqDist
-from itertools import chain, ifilter
-import pandas as pd
-
 def bigrams_excluding(text, stopwords):
     """Extract bigrams from text, returning only those that don't
     incude words occuring stopwords.
@@ -13,9 +8,7 @@ def bigrams_excluding(text, stopwords):
     >>> list(homework.bigrams_excluding(text, stopwords))
     [('after', 'all'), ('all', 'is'), ('is', 'said')]
     """
-    return ifilter(lambda (w1, w2): w1 not in stopwords and w2 not in
-                   stopwords,
-                   nl_bigrams(text))
+    pass
 
 def most_common_bigrams_excluding(corpus, stopwords, n):
     """Given a corpus of text and a number of stopwords, return the n
@@ -29,8 +22,7 @@ def most_common_bigrams_excluding(corpus, stopwords, n):
     >>> homework.most_common_bigrams_excluding(corpus, stopwords, 2)
     [(('after', 'all'), 2), (('all', 'is'), 1)]
     """
-    return FreqDist(chain(*[bigrams_excluding(text, stopwords) for
-                            text in corpus])).most_common(n)
+    pass
 
 def make_bigram_df(corpus, bigrams):
     """Given a corpus of text, make a pandas data frame that contains
@@ -47,10 +39,7 @@ def make_bigram_df(corpus, bigrams):
     0  True   True
     1  True  False
     """
-    corpus_bigrams = [set(nl_bigrams(t)) for t in corpus]
-    return pd.DataFrame({bigram: [bigram in text_bigram for
-                                  text_bigram in corpus_bigrams]
-                         for bigram in bigrams})
+    pass
 
 def make_bigram_classifier(pos_corpus, neg_corpus, base_classifier, n_bigrams):
     """Given a corpus of positive and negatve texts, make a classifier
@@ -68,17 +57,4 @@ def make_bigram_classifier(pos_corpus, neg_corpus, base_classifier, n_bigrams):
                                             negative[:800],
                                             GaussianNB(), 100)
     """
-    from nltk.corpus import stopwords
-    en_stop = set(stopwords.words('english'))
-    en_stop.update(set(',."();:?-!'))
-    bigrams = most_common_bigrams_excluding(chain(pos_corpus,
-                                                  neg_corpus),
-                                            en_stop,
-                                            n_bigrams)
-    bigrams = [b for b, c in bigrams]
-    pos_df = make_bigram_df(pos_corpus, bigrams)
-    neg_df = make_bigram_df(neg_corpus, bigrams)
-    pos_df['sentiment'] = 1
-    neg_df['sentiment'] = 0
-    full_df = pos_df.append(neg_df)
-    return base_classifier.fit(full_df[bigrams], full_df['sentiment']), bigrams
+    pass
